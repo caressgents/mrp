6319da5a4be63e5b85faee716c0b6369f625479e
caressgents <m6banger@gmail.com>
2023-12-08 19:02:35 +0000

    Add debugging and extension check to file upload filter

---
+++ src/middleware/fileUploadMiddleware.js
@@ -0,0 +1,27 @@
+const multer = require('multer');
+const path = require('path');
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, path.join(__dirname, '..', 'uploads'));
+  },
+  filename: (req, file, cb) => {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+    cb(null, file.fieldname + '-' + uniqueSuffix + path.extname(file.originalname));
+  }
+});
+
+const fileFilter = (req, file, cb) => {
+  const fileExtension = path.extname(file.originalname).toLowerCase();
+  console.log('Uploading file with mimetype:', file.mimetype, 'and extension:', fileExtension);
+
+  if (file.mimetype.includes('csv') || fileExtension === '.csv') {
+    cb(null, true);
+  } else {
+    cb(new Error('Only .csv files are allowed!'), false);
+  }
+};
+
+const upload = multer({ storage: storage, fileFilter: fileFilter });
+
+module.exports = upload;
