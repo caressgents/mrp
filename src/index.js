69bc80072b3e0fc17e434eea26d7fc72e75d2009
caressgents <m6banger@gmail.com>
2023-12-08 14:23:26 +0000

    Initial Commit

---
+++ src/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const compression = require('compression');
+const helmet = require('helmet');
+const winston = require('winston');
+
+require('dotenv').config();
+
+const app = express();
+
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.error('MongoDB connection error:', err));
+
+app.use(bodyParser.json());
+app.use(compression());
+app.use(helmet());
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+const logger = winston.createLogger({
+  // winston configuration here
+});
